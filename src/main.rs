ffa5af20e7f0d6d77900b21dfd8f23a57b710626
Edson Michaque <edson@michaque.com>
2023-03-24 17:43:35 +0200

    Create BuildOptions type

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let builder = GoBuilder {};
 
-    let builder = GoBuilder {
-        file: String::from("cmd/foo/main.go"),
-        _before_build: Some(String::from("Hello world")),
-        _after_build: None,
+    let opts = BuildOptions {
+        work_dir: None,
+        id: String::from("linux"),
+        os: OS::Windows,
+        arch: Arch::Arm64(Arm64::V7),
+        target: String::from("foo"),
+        setup: None,
+        teardown: None,
+        files: Some(Vec::from([String::from("cmd/foo/main.go")])),
     };
 
-    match builder.build(
-        String::from("linux"),
-        String::from("bin/foo"),
-        OS::OpenBSD,
-        Arch::Arm64(Arm64::V6),
-    ) {
+    match builder.build(&opts) {
         Ok(build) => println!("{:?}", build),
         Err(_) => println!("boom"),
     };
@@ -21,14 +21,12 @@ fn main() {
 #[derive(Debug)]
 pub struct Build {
     pub command: String,
+    pub target: String,
 }
 
 pub struct Error;
 
-pub struct Package;
-
-pub struct Publish;
-
+#[derive(Debug)]
 pub enum Arch {
     Amd64,
     X86,
@@ -36,11 +34,13 @@ pub enum Arch {
     Arm,
 }
 
+#[derive(Debug)]
 pub enum Arm64 {
     V6,
     V7,
 }
 
+#[derive(Debug)]
 pub enum OS {
     Linux,
     Windows,
@@ -51,38 +51,37 @@ pub enum OS {
 }
 
 pub trait Builder {
-    fn before_build(&self) {
-        println!("before build")
-    }
-
-    fn build(&self, id: String, target: String, os: OS, arch: Arch) -> Result<Build, Error>;
-
-    fn after_build(&self) {
-        println!("after build")
-    }
+    fn build(&self, opts: &BuildOptions) -> Result<Build, Error>;
 }
 
-pub struct GoBuilder {
-    pub file: String,
-    pub _before_build: Option<String>,
-    pub _after_build: Option<String>,
+const LINUX: &'static str = "linux";
+const WINDOWS: &'static str = "windows";
+const DARWIN: &'static str = "darwin";
+const FREEBSD: &'static str = "freebsd";
+const OPENBSD: &'static str = "openbsd";
+const NETBSD: &'static str = "netbsd";
+
+pub struct GoBuilder {}
+
+#[derive(Debug)]
+pub struct BuildOptions {
+    pub work_dir: Option<String>,
+    pub id: String,
+    pub target: String,
+    pub os: OS,
+    pub arch: Arch,
+    pub setup: Option<String>,
+    pub teardown: Option<String>,
+    pub files: Option<Vec<String>>,
 }
 
 impl Builder for GoBuilder {
-    fn build(&self, _: String, target: String, os: OS, arch: Arch) -> Result<Build, Error> {
-        if let Some(s) = &self._before_build {
+    fn build(&self, opts: &BuildOptions) -> Result<Build, Error> {
+        if let Some(s) = &opts.setup {
             println!("{}", s);
-            self.before_build();
         }
 
-        const LINUX: &'static str = "linux";
-        const WINDOWS: &'static str = "windows";
-        const DARWIN: &'static str = "darwin";
-        const FREEBSD: &'static str = "freebsd";
-        const OPENBSD: &'static str = "openbsd";
-        const NETBSD: &'static str = "netbsd";
-
-        let os_str = match os {
+        let build_os = match opts.os {
             OS::Linux => LINUX,
             OS::Windows => WINDOWS,
             OS::Darwin => DARWIN,
@@ -91,31 +90,41 @@ impl Builder for GoBuilder {
             OS::NetBSD => NETBSD,
         };
 
-        let arch_str = match arch {
+        let build_arch = match &opts.arch {
             Arch::Amd64 => "amd64",
             Arch::X86 => "386",
             Arch::Arm => "arm",
-            Arch::Arm64(v) => match v {
-                Arm64::V6 => "arm64",
-                Arm64::V7 => "arm64",
-            },
+            Arch::Arm64(_) => "arm64",
         };
 
-        let mut target = format!("{}_{}_{}", target, os_str, arch_str);
+        let mut build_target = format!("{}_{}_{}", opts.target, build_os, build_arch);
 
-        if let OS::Windows = os {
-            target = format!("{}.exe", target,)
+        if let OS::Windows = opts.os {
+            build_target = format!("{}.exe", build_target)
         }
 
-        if let Some(_) = self._after_build {
-            self.after_build()
+        if let Some(s) = &opts.teardown {
+            println!("{}", s);
+        }
+
+        let mut build_file = String::from("main.go");
+
+        if let Some(files) = &opts.files {
+            if files.len() != 0 {
+                build_file = String::from("");
+            }
+
+            for file in files {
+                build_file = format!("{} {}", build_file, file)
+            }
         }
 
         Ok(Build {
             command: format!(
                 "GOOS={} GOARCH={} go build -o {} {}",
-                os_str, arch_str, target, self.file
+                build_os, build_arch, build_target, build_file
             ),
+            target: build_target,
         })
     }
 }
