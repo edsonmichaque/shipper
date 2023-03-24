10960bede337ae54870f58a915eda6be89c706c9
Edson Michaque <edson@michaque.com>
2023-03-24 16:31:29 +0200

    Add build lifecycle hooks

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,16 @@ fn main() {
 
     let builder = GoBuilder {
         file: String::from("cmd/foo/main.go"),
+        _before_build: Some(String::from("Hello world")),
+        _after_build: None,
     };
 
-    match builder.build(String::from("linux"), OS::NetBSD, Arch::Arm64(Arm64::V6)) {
+    match builder.build(
+        String::from("linux"),
+        String::from("bin/foo"),
+        OS::OpenBSD,
+        Arch::Arm64(Arm64::V6),
+    ) {
         Ok(build) => println!("{:?}", build),
         Err(_) => println!("boom"),
     };
@@ -44,22 +51,44 @@ pub enum OS {
 }
 
 pub trait Builder {
-    fn build(&self, id: String, os: OS, arch: Arch) -> Result<Build, Error>;
+    fn before_build(&self) {
+        println!("before build")
+    }
+
+    fn build(&self, id: String, target: String, os: OS, arch: Arch) -> Result<Build, Error>;
+
+    fn after_build(&self) {
+        println!("after build")
+    }
 }
 
 pub struct GoBuilder {
     pub file: String,
+    pub _before_build: Option<String>,
+    pub _after_build: Option<String>,
 }
 
 impl Builder for GoBuilder {
-    fn build(&self, _: String, os: OS, arch: Arch) -> Result<Build, Error> {
+    fn build(&self, _: String, target: String, os: OS, arch: Arch) -> Result<Build, Error> {
+        if let Some(s) = &self._before_build {
+            println!("{}", s);
+            self.before_build();
+        }
+
+        const LINUX: &'static str = "linux";
+        const WINDOWS: &'static str = "windows";
+        const DARWIN: &'static str = "darwin";
+        const FREEBSD: &'static str = "freebsd";
+        const OPENBSD: &'static str = "openbsd";
+        const NETBSD: &'static str = "netbsd";
+
         let os_str = match os {
-            OS::Linux => "linux",
-            OS::Windows => "windows",
-            OS::Darwin => "darwin",
-            OS::FreeBSD => "freebsd",
-            OS::OpenBSD => "openbsd",
-            OS::NetBSD => return Err(Error),
+            OS::Linux => LINUX,
+            OS::Windows => WINDOWS,
+            OS::Darwin => DARWIN,
+            OS::FreeBSD => FREEBSD,
+            OS::OpenBSD => OPENBSD,
+            OS::NetBSD => NETBSD,
         };
 
         let arch_str = match arch {
@@ -72,8 +101,21 @@ impl Builder for GoBuilder {
             },
         };
 
+        let mut target = format!("{}_{}_{}", target, os_str, arch_str);
+
+        if let OS::Windows = os {
+            target = format!("{}.exe", target,)
+        }
+
+        if let Some(_) = self._after_build {
+            self.after_build()
+        }
+
         Ok(Build {
-            command: format!("GOOS={} GOARCH={} go build {}", os_str, arch_str, self.file),
+            command: format!(
+                "GOOS={} GOARCH={} go build -o {} {}",
+                os_str, arch_str, target, self.file
+            ),
         })
     }
 }
